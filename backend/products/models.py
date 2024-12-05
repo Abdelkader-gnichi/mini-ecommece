17999fe814bbac2c8417da1b06ce8d92aae22d90
Abdelkader-gnichi <ggnichi097@gmail.com>
2024-12-05 15:00:28 +0100

    setting up two test cases (crete a product, fetch all products)

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from decimal import Decimal
 # Create your models here.
 
 
@@ -10,7 +11,7 @@ class Product(models.Model):
     price = models.DecimalField(
         max_digits=10, 
         decimal_places=2, 
-        validators=[MinValueValidator(0)]
+        validators=[MinValueValidator(Decimal('0.00'))]
     )
     stock = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
